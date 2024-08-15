3e8d7ed3d9ae30e1b93554ed32ca740b427d8558
Arie <arieinjector@gmail.com>
2024-08-15 10:33:54 +0700

    feat(fix): timeout for anime-list API

--- src/routes/anime_list/index.ts
+++ src/routes/anime_list/index.ts
@@ -6,13 +6,19 @@ import { animeList } from "../../libs/scrape_anime_list";
 const animeListRoute = new Hono();
 
 animeListRoute.get('/', async (c) => {
-    const { data } = await axios.get(`${process.env.OTAKUDESU_URL}/anime-list`);
-    const $ = load(data);
-
-    const animeListScrape = $('#abtext .bariskelom').toString();
-    const animeListResult = animeList(animeListScrape);
+    try {
+        const { data } = await axios.get(`${process.env.OTAKUDESU_URL}/anime-list`, { timeout: 10000 });
+        const $ = load(data);
     
-    return c.json(animeListResult, 200);
+        const animeListScrape = $('#abtext .bariskelom').toString();
+        const animeListResult = animeList(animeListScrape);
+        
+        return c.json(animeListResult, 200);
+    } catch (error) {
+        return c.json({
+            message: "Request timeout :("
+        }, 504);
+    }
 });
 
 export default animeListRoute;
